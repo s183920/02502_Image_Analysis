508703660375f343260e199e3bdc90ba2bed0e47
Lukas Leindals <lukas@leindals.dk>
2021-05-16 18:42:02 +0200

    landmark data support

--- imgAnalysis.py
+++ imgAnalysis.py
@@ -2,7 +2,7 @@ import win32clipboard
 import re
 import time
 
-def img_to_mat(rgb = False):
+def img_to_mat(type_of_data = "gray"):
     # get clipboard data
     win32clipboard.OpenClipboard()
     data = win32clipboard.GetClipboardData()
@@ -10,7 +10,7 @@ def img_to_mat(rgb = False):
     data_org = data
 
     # clean data
-    if rgb:
+    if type_of_data == "rgb":
         # data = data.upper()
         data = re.sub(r"[^\d\&\\RGB]", "", data)
         data = [t for t in data.split("\\\\") if bool(re.search(r'\d', t))]
@@ -38,18 +38,30 @@ def img_to_mat(rgb = False):
         
         # concat data
         data = R + ";\n" + G + ";\n" + B + ";\nim_rgb = cat(3, r, g, b)"
-    else:
+    elif type_of_data == "gray":
         data = re.sub(r"[^\d\&\\]", "", data)
         data = [t for t in data.split("\\") if bool(re.search(r'\d', t))]
         data = ";\n".join(data)
         data = re.sub(r"\&", ", ", data)
         data = "[" + data + "]"
+    elif type_of_data == "landmark":
+        data = re.sub(r"[^\d\&\\ab]", "", data)
+        
+        # test = [re.sub(r"[^\d&ab]", "", t) for t in test.split("\\\\") if bool(re.search(r'\d', t))]
+        out = ""
+        data = re.sub(r"[\&]", " ", data)
+        data = re.findall("\w\d{1} \d{1,2} \d{1,2}", data)
+
+        for t in data:
+            t = t.split(" ")
+            out += t[0] + " = [" + t[1] + "; " + t[2] + "];\n"
+        
+        data = out
 
     # set clipboard data
     if data_org == data:
         print("The copied data is unchanged. Please make sure data is copied in a LaTeX format")
-    else:
-        
+    else:       
         win32clipboard.OpenClipboard()
         win32clipboard.EmptyClipboard()
         win32clipboard.SetClipboardText(data)
@@ -62,9 +74,12 @@ def img_to_mat(rgb = False):
 
 if __name__ == "__main__":
     print("Please enter the number of the type of data you have in your clip board")
-    print("\t1: RGB table\n\t2: Grey level image")
+    print("\t1: RGB table\n\t2: Grey level image\n\t3: Landmark data")
     type_of_data = int(input("Datatype: "))
-    assert type_of_data in [1,2], "The given input was not an option"
-    rgb = True if type_of_data == 1 else False
-    img_to_mat(rgb)
+    assert type_of_data in [1,2,3], "The given input was not an option"
+    datatype = {1:"rgb", 2:"gray", 3:"landmark"}
+    img_to_mat(datatype[type_of_data])
+
+
+
 
